1755f4d2e614949bbcbd40017e697b1caf74e071
Emirhan YURT <emirhanyurt.py@gmail.com>
2022-10-12 22:32:07 +0300

    form and ngif example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AngularExercise';
-  value:string=""
-  /**
-   *
-   */
-  buttonDisable = true
-  constructor() {
-    setTimeout(()=>
-    {
-      this.buttonDisable = false
-    },2000)
-    
-    
-}
-buttonDisabled()
-    {
-      this.buttonDisable = true
-    }
-    getValue()
-    {
-        console.log(this.value)
-    }
+ email:string = ""
+ password:string =""
+ check:boolean = false
+ button:boolean = false
+
+
+ checkAvaible(){
+  if(this.email != "" && this.password != "" && this.check )
+  {
+    this.button = true
+  }
+  else{
+    this.button = false
+  }
+ }
+ changeClass(value:string)
+ {
+  if(value != "")
+  {
+    return "form-control succesVali";
+  }
+  else
+  {
+    return "form-control errorVali"
+  }
+ }
+
   }
